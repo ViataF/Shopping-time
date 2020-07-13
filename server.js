44cb76e226f7fad65bf1aa622a914a475b60d711
ViataF <viataruth@gmail.com>
2020-07-13 13:09:03 +0200

    dependecies & express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the ShoppingTime API" })
+);
+
+// Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/places", require("./routes/places"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
