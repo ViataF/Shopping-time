c85adb9cdff8e8ce8d6acf4c54e871450841ab0b
ViataF <viataruth@gmail.com>
2020-07-13 20:08:34 +0200

    Add places route

--- routes/places.js
+++ routes/places.js
@@ -19,8 +19,49 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+// // @route   Post api/info
+// @desc    Add info
+// @access  Public
+router.post(
+  "/",
+  [auth, [check("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      name,
+      email,
+      phone,
+      businessName,
+      category,
+      description,
+    } = req.body;
+
+    try {
+      const addInfo = new Places({
+        name,
+        email,
+        phone,
+        businessName,
+        category,
+        description,
+        user: req.user.id,
+      });
+
+      const info = await addInfo.save();
+
+      res.json(info);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 // @route   PUT api/info
-// @desc    Update contact
+// @desc    Update info
 // @access  Public
 router.put("/", (req, res) => {
   res.send("Update info");
