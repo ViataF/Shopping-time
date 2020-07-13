b44860bc2bffc5079dda0e1eac196078c6f6e8a9
ViataF <viataruth@gmail.com>
2020-07-13 20:28:52 +0200

    Info delete and update routes.

--- routes/places.js
+++ routes/places.js
@@ -63,8 +63,63 @@ router.post(
 // @route   PUT api/info
 // @desc    Update info
 // @access  Public
-router.put("/", (req, res) => {
-  res.send("Update info");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, businessName, category, description } = req.body;
+
+  //Build places object
+  const placesFields = {};
+  if (name) placesFields.name = name;
+  if (email) placesFields.email = email;
+  if (phone) placesFields.phone = phone;
+  if (businessName) placesFields.businessName = businessName;
+  if (category) placesFields.category = category;
+  if (description) placesFields.description = description;
+
+  try {
+    let info = await Places.findById(req.params.id);
+
+    if (!info) return res.status(404).json({ msg: "Info not found" });
+
+    //  Make sure user owns contact
+    if (info.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    info = await Places.findByIdAndUpdate(
+      req.params.id,
+      { $set: placesFields },
+      { new: true }
+    );
+
+    res.json(info);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   Delete api/info/:id
+// @desc    Delete info
+// @access  Private
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let info = await Places.findById(req.params.id);
+
+    if (!info) return res.status(404).json({ msg: "Info not found" });
+
+    //  Make sure user owns contact
+    if (info.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    await Places.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: "Info removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
