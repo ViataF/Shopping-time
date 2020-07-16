160c8b1b3d92c28d03fec08d7cadfc216d001521
ViataF <viataruth@gmail.com>
2020-07-16 03:30:10 +0200

    added navbar and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 import UserInfoState from "./context/user_info/UserInfoState";
 import "./App.css";
@@ -12,14 +13,16 @@ const App = () => {
     <UserInfoState>
       <Router>
         <Fragment>
-          <Navbar />
           <div className="container">
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/about" component={About} />
-              <Route exact path="/" component={Home} />
+              <Route exact path="/contact" component={Contact} />
             </Switch>
           </div>
+          <Navbar />
+          <About />
+          <Contact />
         </Fragment>
       </Router>
     </UserInfoState>
