bb2eb2868985f03e69102759c9255f716695e793
ViataF <viataruth@gmail.com>
2020-07-16 03:18:37 +0200

    bootstrap navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,32 +1,65 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
+import "./Navbar.css";
+import { Navbar, Nav } from "react-bootstrap";
 
-const Navbar = ({ title, icon }) => {
+const Navigation = () => {
   return (
-    <div className="navbar bg-primary">
-      <h1>
-        <i className={icon} /> {title}
-      </h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </div>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      sticky={sticky}
+    >
+      <Navbar.Brand href="/">Shopping Time</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="m-auto">
+          <Link
+            className="nav-link"
+            activeClass="active"
+            to="home"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            Home
+          </Link>
+          <Link
+            className="nav-link"
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-55}
+            duration={500}
+          >
+            About
+          </Link>
+          <Link
+            className="nav-link"
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-55}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-Navbar.propTypes = {
-  title: PropTypes.string.isRequired,
-  icon: PropTypes.string,
-};
-
-Navbar.defaultProps = {
-  title: "ShoppingTimes",
-  icon: "i",
-};
-export default Navbar;
+export default Navigation;
