c9b8b7eebd64e6b79032bd49d1430107087a4c10
ViataF <viataruth@gmail.com>
2020-07-23 04:43:59 +0200

    Added alert remover

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -6,6 +6,7 @@ export const UPDATE_INFO = "UPDATE_INFO";
 export const FILTER_INFO = "FILTER_INFO";
 export const CLEAR_FILTER = "CLEAR_FILTER";
 export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
 
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
