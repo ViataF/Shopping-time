4bd02d6541cc32f1beaa73705239c9432285a27b
ViataF <viataruth@gmail.com>
2020-07-23 21:03:22 +0200

    Load user & set token

--- client/src/App.js
+++ client/src/App.js
@@ -3,32 +3,48 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Alerts from "./components/layout/Alert";
 import Contact from "./components/pages/Contact";
 
-import "antd/dist/antd.css";
 import UserInfoState from "./context/user_info/UserInfoState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import setAuthToken from "./utils/setAuhToken";
 import "./App.css";
+import "antd/dist/antd.css";
 import UserForm from "./components/Userinfo/UserForm";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 const App = () => {
   return (
-    <UserInfoState>
-      <Router>
-        {/* <Navbar /> */}
-        <Fragment>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/contact" component={Contact} />
-              <Route exact path="/userform" component={UserForm} />
-            </Switch>
-            <About />
-            <Contact />
-          </div>
-        </Fragment>
-      </Router>
-    </UserInfoState>
+    <AuthState>
+      <UserInfoState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              {/* <Navbar /> */}
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/contact" component={Contact} />
+                  <Route exact path="/userform" component={UserForm} />
+                </Switch>
+                <About />
+                <Contact />
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </UserInfoState>
+    </AuthState>
   );
 };
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,26 @@
-import React from "react";
-
+import React, { Fragment, useContext, useEffect } from "react";
+import Slideshow from "../layout/Slideshow";
+import Users from "../Userinfo/UserInfo";
+import UserForm from "../Userinfo/UserForm";
+import CategorieFilter from "../Userinfo/CategorieFilter";
+import AuthContext from "../../context/auth/authContext";
+import "./Home.css";
 const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="home-section" id="home">
-      <h1>Home</h1>
+      <Fragment>
+        <Slideshow />
+        <Users />
+        <UserForm />
+        <CategorieFilter />
+      </Fragment>
     </div>
   );
 };
