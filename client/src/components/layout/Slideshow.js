fdc74d8a72b96c90a8b72e12d6773558b8c6ea75
ViataF <viataruth@gmail.com>
2020-08-01 21:54:18 +0200

    Slideshow and styling

--- client/src/components/layout/Slideshow.js
+++ client/src/components/layout/Slideshow.js
@@ -4,25 +4,27 @@ import "react-slideshow-image/dist/styles.css";
 import "./Slideshow.css";
 const Slideshow = () => {
   return (
-    <div className="slide-container">
-      <Slide>
+    <div className="slideshow ">
+      <Slide arrows={false} indicators={true}>
         <div className="each-slide1">
           <section>
-            <h2>Shopping Time</h2>
-            <p>Slide 1</p>
+            <h2>
+              We are <span>Seekers & Finders</span>
+            </h2>
+            <p>Explore using Seekers&Finders</p>
           </section>
         </div>
 
-        <div className="each-slide2">
+        <div className="each-slide2 white-text">
           <section>
-            <h2>Shopping Time</h2>
-            <p>Slide 1</p>
+            <h2 className="white-text">Directory</h2>
+            <p>You can discover new outlets</p>
           </section>
         </div>
         <div className="each-slide3">
           <section>
-            <h2>Shopping Time</h2>
-            <p>Slide 1</p>
+            <h2>Convenient and Informative</h2>
+            <p>Stay updated with trendy stores</p>
           </section>
         </div>
       </Slide>
