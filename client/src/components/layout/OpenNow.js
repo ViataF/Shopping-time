8b1b4e87c709abb264cb1948693abdfa9b87379d
ViataF <viataruth@gmail.com>
2020-08-01 21:53:44 +0200

    Open now fetch from api

--- client/src/components/layout/OpenNow.js
+++ client/src/components/layout/OpenNow.js
@@ -4,10 +4,13 @@ import BusinessInfo from "../Userinfo/BusinessInfo";
 const OpenNow = () => {
   return (
     <div className="container-openNow">
-      <div className="openNow">
-        <h2 className="openNow">Open Now</h2>
-        <BusinessInfo />
-        <a href="/shops">Show more</a>
+      <h3 className="openNow">Open Now</h3>
+
+      <BusinessInfo />
+      <div className="button">
+        <a href="/shops" className="waves-effect waves-light btn blue ">
+          Show more
+        </a>
       </div>
     </div>
   );
