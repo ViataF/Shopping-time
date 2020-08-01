9591b0167387cfc8e0a023635e7936d93a18e9c8
ViataF <viataruth@gmail.com>
2020-08-01 21:54:39 +0200

    Styling and components

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Purpose from "../layout/Purpose";
 
 const About = () => {
   return (
     <div className="about-section" id="about">
-      <h1> About us</h1>
-      <p>ddddd</p>
+      <h1>We are Seekers & Finders</h1>
+      <p>
+        Connecting businessses seeking for customers and customers seeking for
+        businesses
+      </p>
+      <section>
+        We have started this organisation to help businesses and customers
+        connect easier and to promote businesses
+      </section>
+      <Purpose />
+      <section></section>
     </div>
   );
 };
