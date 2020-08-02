45af6f7dc76cf431329bf1a4f0a1da0385715d76
ViataF <viataruth@gmail.com>
2020-08-02 15:37:47 +0200

    Css styling

--- client/src/components/Userinfo/UserForm.js
+++ client/src/components/Userinfo/UserForm.js
@@ -75,129 +75,131 @@ const UserForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <h2 className="black-text">
-        {current ? "Edit Information" : "Add Information"}
-      </h2>
-      <label>Name</label>
-      <input
-        type="text"
-        placeholder="Name"
-        name="name"
-        value={name}
-        onChange={onChange}
-      />{" "}
-      <label>Email</label>
-      <input
-        type="email"
-        placeholder="Email"
-        name="email"
-        value={email}
-        onChange={onChange}
-      />{" "}
-      <label>Name</label>
-      <input
-        type="text"
-        placeholder="Phone"
-        name="phone"
-        value={phone}
-        onChange={onChange}
-      />{" "}
-      <label>Business Name</label>
-      <input
-        type="text"
-        placeholder="BusinessName"
-        name="businessName"
-        value={businessName}
-        onChange={onChange}
-      />{" "}
-      <label>Description</label>
-      <input
-        type="text"
-        placeholder="Description"
-        name="description"
-        value={description}
-        onChange={onChange}
-      />{" "}
-      <label>Address</label>
-      <input
-        type="text"
-        placeholder="Address"
-        name="address"
-        value={address}
-        onChange={onChange}
-      />
-      <h5>Categories</h5>
-      <select
-        id="category"
-        name="category"
-        value={category}
-        onChange={onChange}
-      >
-        {" "}
-        <option value="catergory">Category</option>
-        <option>Accessories</option>
-        <option>Clothing</option>
-        <option>Shop</option>
-        <option>Grocery store</option>
-        <option>House&Home</option>
-        <option>Malls</option>
-        <option>Restaurants</option>
-        <option>Cellphone shops</option>
-      </select>
-      <div name="open" onChange={onChange}>
-        <label>Open time</label>
+    <div className="userform">
+      <form onSubmit={onSubmit}>
+        <h2 className="black-text center">
+          {current ? "Edit Information" : "Add Information"}
+        </h2>
+        <label>Name</label>
         <input
-          type="time"
-          id="appt"
-          name="open"
-          value={open}
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={name}
           onChange={onChange}
-          required
-        />
-        <label htmlFor="">Close time</label>
+        />{" "}
+        <label>Email</label>
         <input
-          type="time"
-          id="appt"
-          name="closed"
-          required
-          value={closed}
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={email}
+          onChange={onChange}
+        />{" "}
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Phone"
+          name="phone"
+          value={phone}
+          onChange={onChange}
+        />{" "}
+        <label>Business Name</label>
+        <input
+          type="text"
+          placeholder="BusinessName"
+          name="businessName"
+          value={businessName}
+          onChange={onChange}
+        />{" "}
+        <label>Description</label>
+        <input
+          type="text"
+          placeholder="Description"
+          name="description"
+          value={description}
+          onChange={onChange}
+        />{" "}
+        <label>Address</label>
+        <input
+          type="text"
+          placeholder="Address"
+          name="address"
+          value={address}
           onChange={onChange}
         />
-        <label htmlFor="">Website URL (optional)</label>
-        <input
-          type="url"
-          id="URL"
-          name="businessURL"
-          required
-          value={businessURL}
+        <h5>Categories</h5>
+        <select
+          id="category"
+          name="category"
+          value={category}
           onChange={onChange}
-        />
-        <label htmlFor="">Logo Url</label>
-        <input
-          type="url"
-          id="logo"
-          name="logo"
-          required
-          value={logo}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <input
-          type="submit"
-          value={current ? "Update Information" : "Add Information"}
-          className="btn"
-        />
-      </div>
-      {current && (
-        <div>
-          <button className="btn" onClick={clearAll}>
-            Clear
-          </button>
+        >
+          {" "}
+          <option value="catergory">Category</option>
+          <option>Accessories</option>
+          <option>Clothing</option>
+          <option>Shop</option>
+          <option>Grocery store</option>
+          <option>House&Home</option>
+          <option>Malls</option>
+          <option>Restaurants</option>
+          <option>Cellphone shops</option>
+        </select>
+        <div name="open" onChange={onChange}>
+          <label>Open time</label>
+          <input
+            type="time"
+            id="appt"
+            name="open"
+            value={open}
+            onChange={onChange}
+            required
+          />
+          <label htmlFor="">Close time</label>
+          <input
+            type="time"
+            id="appt"
+            name="closed"
+            required
+            value={closed}
+            onChange={onChange}
+          />
+          <label htmlFor="">Website URL (optional)</label>
+          <input
+            type="url"
+            id="URL"
+            name="businessURL"
+            required
+            value={businessURL}
+            onChange={onChange}
+          />
+          <label htmlFor="">Logo Url</label>
+          <input
+            type="url"
+            id="logo"
+            name="logo"
+            required
+            value={logo}
+            onChange={onChange}
+          />
         </div>
-      )}
-    </form>
+        <div>
+          <input
+            type="submit"
+            value={current ? "Update Information" : "Add Information"}
+            className="btn"
+          />
+        </div>
+        {current && (
+          <div>
+            <button className="btn" onClick={clearAll}>
+              Clear
+            </button>
+          </div>
+        )}
+      </form>
+    </div>
   );
 };
 
